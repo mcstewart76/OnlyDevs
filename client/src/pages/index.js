fbdda2255e249ab836172d700d31799b48a7fbee
mcstewart76 <mcstewart76@gmail.com>
2022-04-06 23:21:13 -0400

    fixed loginpage UI and css

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap'
+import { Form, Button} from 'react-bootstrap'
 import Odie from '../OdieLogo2.0.png'
 
 const Login = () => {
   return (
     <>
-      <body>
-        <Container className='loginform'>
+      <body >
+        <div className='d-flex justify-content-center mx-4 bg-dark'>
+        <div className='loginform my-5 row p-4 align-items-center justify-content-center'>
           <img className='odieIMG' src={Odie} alt="OdieTheDev" />
-          <Form className='formaspects'>
+          <Form className='formaspects px-2 mx-2'>
             <Form.Group className="mb-3" controlId="formUsername">
               <Form.Label>Username</Form.Label>
               <Form.Control type="username" placeholder="Username" />
@@ -21,11 +22,12 @@ const Login = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <Button className='libtn' variant="primary" type="submit">
+            <Button className='libtn ' variant="primary" type="submit">
               Login
             </Button>
           </Form>
-        </Container>
+        </div>
+        </div>
       </body>
     </>
   );
