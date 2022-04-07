d0d83fb93b89cbcaafb296047d49907cb2c904b1
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-04-06 21:08:27 -0400

    Started Login Page
    Started the login page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap'
+import Odie from '../OdieLogo2.0.png'
 
 const Login = () => {
   return (
     <>
-      <div>
-        <div>
-          <h1>Login/Registration</h1>
-          <h1>Pull up a chair</h1>
-          <h1>Stay awhile</h1>
-        </div>
-          
-      </div>
+      <body>
+        <Container className='loginform'>
+          <img className='odieIMG' src={Odie} alt="OdieTheDev" />
+          <Form className='formaspects'>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Username" />
+              <Form.Text className="text-muted">
 
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button className='libtn' variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Container>
+      </body>
     </>
   );
 };
