48619a876756785aa5cf3979399daca90cc7d385
mcstewart76 <mcstewart76@gmail.com>
2022-04-07 20:12:03 -0400

    Added registration, updated navbar style just a bit

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/home';
 import Profile from './pages/profile';
 import Message from './pages/message';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Registration from './pages/Registration';
 
 
 function App() {
@@ -15,6 +16,7 @@ return (
 	<Navigation />
 	<Routes>
 		<Route exact path='/' element={<Login />} />
+		<Route path='/Registration' element={<Registration />} />
 		<Route path='/home' element={<Home/>} />
     	<Route path='/profile' element={<Profile/>} />
 		<Route path='/message' element={<Message/>} />
