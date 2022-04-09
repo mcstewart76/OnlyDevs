5e081c13ce04ec07e94281e4aeafb8a39cb1b50c
mcstewart76 <mcstewart76@gmail.com>
2022-04-09 13:16:50 -0400

    Added profile page, and updated css

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,21 +4,34 @@ import { Card, } from 'react-bootstrap'
 const Profile = () => {
   return (
     <>
-      <div className='text-white d-flex flex-wrap p-2 mx-auto flex-items-center justify-content-center  container'>
-        <div className='sidebarLay boxOut d-flex mb-5'>
-        <Card style={{ width: '18rem' }}>
+      <div className='text-white d-flex flex-wrap p-2 mx-auto flex-items-center justify-content-center container'>
+        <div className='sidebarLay boxOut d-flex m-4'>
+          <Card >
             <Card.Body>
-              <Card.Title className='text-dark'>Sidebar Card</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Side</Card.Subtitle>
+              <div className='devCard text-dark p-2 '>
+                <div className='profile '>
+                <div className="profile-picture d-flex">
+                  <img id="propic" src="https://avatars.githubusercontent.com/u/90533949?v=4" alt="profilepicture" className="rounded-circle rounded mx-auto d-block"></img>
+                </div>
+                <Card.Title className='text-dark d-flex'>Sidebar Card</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted d-flex">Side</Card.Subtitle>
+                </div>
+                <div className="biocard p-3 " >I love to build and design things related to engineering or tech!
+                  Check out some of my repos!
+                </div>
+                
+              </div>
+              
+              
 
             </Card.Body>
           </Card>
 
         </div>
-        <div className='mainLay boxOut p-2 d-flex flex-wrap justify-content-center '>
+        <div className='mainLay boxOut p-2 d-flex flex-wrap justify-content-center m-4 '>
 
-            <div className=' d-flex justify-content-center flex-wrap px-3'>
-              <Card className='m-2' style={{ width: '18rem' }}>
+          <div className=' d-flex justify-content-center flex-wrap px-3'>
+            <Card className='m-2' style={{ width: '18rem' }}>
               <Card.Body>
                 <Card.Title className='text-dark'>Sample Repo</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
@@ -33,10 +46,10 @@ const Profile = () => {
 
               </Card.Body>
             </Card>
-            
-            </div>
-            <div className=' d-flex justify-content-center flex-wrap px-3'>
-              <Card className='m-2 ' style={{ width: '18rem' }}>
+
+          </div>
+          <div className=' d-flex justify-content-center flex-wrap px-3'>
+            <Card className='m-2 ' style={{ width: '18rem' }}>
               <Card.Body>
                 <Card.Title className='text-dark'>Sample Repo</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
@@ -51,10 +64,10 @@ const Profile = () => {
 
               </Card.Body>
             </Card>
-            
-            </div>
-            <div className=' d-flex justify-content-center flex-wrap px-3'>
-              <Card className='m-2 ' style={{ width: '18rem' }}>
+
+          </div>
+          <div className=' d-flex justify-content-center flex-wrap px-3'>
+            <Card className='m-2 ' style={{ width: '18rem' }}>
               <Card.Body>
                 <Card.Title className='text-dark'>Sample Repo</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
@@ -69,8 +82,8 @@ const Profile = () => {
 
               </Card.Body>
             </Card>
-            
-            </div>
+
+          </div>
 
         </div>
 
