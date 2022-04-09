40d58f3bad47db8f807e58cda562f370e9092a14
mcstewart76 <mcstewart76@gmail.com>
2022-04-08 23:02:34 -0400

    Worked on creating flex boxes for repos display

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,15 +1,79 @@
 import React from 'react';
+import { Card, } from 'react-bootstrap'
 
 const Profile = () => {
   return (
     <>
-      <div>
-        <div>
-          <h1>Welcome to the Profile page</h1>
-          <h1>Your personal repos</h1>
-          <h1>Go here</h1>
+      <div className='text-white d-flex flex-wrap p-2 mx-auto flex-items-center justify-content-center  container'>
+        <div className='sidebarLay boxOut d-flex mb-5'>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title className='text-dark'>Sidebar Card</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Side</Card.Subtitle>
+
+            </Card.Body>
+          </Card>
+
         </div>
-          
+        <div className='mainLay boxOut p-2 d-flex flex-wrap justify-content-center '>
+
+            <div className=' d-flex justify-content-center flex-wrap px-3'>
+              <Card className='m-2' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+
+            <Card className='m-2' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+            
+            </div>
+            <div className=' d-flex justify-content-center flex-wrap px-3'>
+              <Card className='m-2 ' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+
+            <Card className='m-2 ' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+            
+            </div>
+            <div className=' d-flex justify-content-center flex-wrap px-3'>
+              <Card className='m-2 ' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+
+            <Card className='m-2 ' style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className='text-dark'>Sample Repo</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Javascript</Card.Subtitle>
+
+              </Card.Body>
+            </Card>
+            
+            </div>
+
+        </div>
+
       </div>
 
     </>
