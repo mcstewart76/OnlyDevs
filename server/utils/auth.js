3c79b5181081e07e389f6f1b4168b8b1a358e7f4
Ernesto Tham <ernestotha@gmail.com>
2022-04-16 09:16:52 -0400

    env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-
-const secret = 'mysecretssshhhhhhh';
+const dotenv = require('dotenv').config();
+const secret =process.env.REACT_APP_SECRETSESH;
 const expiration = '2h';
 
 module.exports = {
